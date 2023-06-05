db4bdf24d2d869e3143f34c1f72116a38cc22962
Mariusz Wróbel <komodo454@wp.pl>
2023-06-05 21:25:17 +0200

    Add add scrolling to registration page

--- lib/pages/register_page/registration_page_widgets/registration_form.dart
+++ lib/pages/register_page/registration_page_widgets/registration_form.dart
@@ -10,71 +10,73 @@ class RegistrationForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(30.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Text(
-              "Register",
-              style: GoogleFonts.rubik(fontSize: 22),
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                "Register",
+                style: GoogleFonts.rubik(fontSize: 22),
+              ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 20, bottom: 30),
-            child: const Column(
+            Container(
+              padding: const EdgeInsets.only(top: 20, bottom: 30),
+              child: const Column(
+                children: [
+                  FormTextField(
+                    hintText: 'Email',
+                    inputIcon: Icons.email,
+                  ),
+                  SpaceBetweenTextFields(),
+                  FormTextField(
+                    hintText: 'Name',
+                    inputIcon: Icons.person,
+                  ),
+                  SpaceBetweenTextFields(),
+                  FormTextField(
+                    hintText: 'Password',
+                    inputIcon: Icons.lock,
+                  ),
+                  SpaceBetweenTextFields(),
+                  FormTextField(
+                    hintText: 'Confirm password',
+                    inputIcon: Icons.lock,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: SubmitButton(
+                buttonText: "Sign up",
+                onButtonPressed: signUpActionButton,
+              ),
+            ),
+            const SpaceBetweenTextFields(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FormTextField(
-                  hintText: 'Email',
-                  inputIcon: Icons.email,
+                Text(
+                  "Have account already? ",
+                  style: GoogleFonts.rubik(fontSize: 14),
                 ),
-                SpaceBetweenTextFields(),
-                FormTextField(
-                  hintText: 'Name',
-                  inputIcon: Icons.person,
-                ),
-                SpaceBetweenTextFields(),
-                FormTextField(
-                  hintText: 'Password',
-                  inputIcon: Icons.lock,
-                ),
-                SpaceBetweenTextFields(),
-                FormTextField(
-                  hintText: 'Confirm password',
-                  inputIcon: Icons.lock,
+                Text(
+                  "Sign in",
+                  style: GoogleFonts.rubik(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 14,
+                  ),
                 ),
               ],
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            height: 60,
-            child: SubmitButton(
-              buttonText: "Sign up",
-              onButtonPressed: signUpActionButton,
-            ),
-          ),
-          const SpaceBetweenTextFields(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Have account already? ",
-                style: GoogleFonts.rubik(fontSize: 14),
-              ),
-              Text(
-                "Sign in",
-                style: GoogleFonts.rubik(
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).colorScheme.secondary,
-                  fontSize: 14,
-                ),
-              ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
