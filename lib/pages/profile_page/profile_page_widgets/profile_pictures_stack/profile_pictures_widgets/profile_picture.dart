e369f9c35f512a9548106a275487cb920eed9829
Mariusz Wróbel <komodo454@wp.pl>
2023-07-24 22:34:32 +0200

    Code refactor

--- lib/pages/profile_page/profile_page_widgets/profile_pictures_stack/profile_pictures_widgets/profile_picture.dart
+++ lib/pages/profile_page/profile_page_widgets/profile_pictures_stack/profile_pictures_widgets/profile_picture.dart
@@ -7,18 +7,20 @@ class ProfilePicture extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
     return Positioned(
-      top: (MediaQuery.of(context).size.width - 60) * 0.3,
+      top: (deviceSize.width - 60) * 0.3,
       child: Container(
-        height: (MediaQuery.of(context).size.width - 60) * 0.4,
-        width: (MediaQuery.of(context).size.width - 60) * 0.4,
+        height: (deviceSize.width - 60) * 0.4,
+        width: (deviceSize.width - 60) * 0.4,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
           image: const DecorationImage(
             fit: BoxFit.cover,
             //TODO image from backend
             image: NetworkImage(
-                'https://zipmex.com/static/d1af016df3c4adadee8d863e54e82331/1bbe7/Twitter-NFT-profile.jpg'),
+              'https://zipmex.com/static/d1af016df3c4adadee8d863e54e82331/1bbe7/Twitter-NFT-profile.jpg',
+            ),
           ),
           border: Border.all(
             color: Theme.of(context).colorScheme.background,
