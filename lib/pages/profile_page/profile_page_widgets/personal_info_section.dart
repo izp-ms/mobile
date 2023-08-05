0a9152716bcaa96d146efff046ee69820329530c
Mariusz Wróbel <komodo454@wp.pl>
2023-08-05 19:08:35 +0200

    Code refactor again

--- lib/pages/profile_page/profile_page_widgets/personal_info_section.dart
+++ lib/pages/profile_page/profile_page_widgets/personal_info_section.dart
@@ -10,67 +10,62 @@ class PersonalInfoSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Flex(
-        direction: Axis.horizontal,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: FittedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Text(
+            "Zdzisław Pierdzipięta",
+            style: GoogleFonts.rubik(
+              fontSize: 28,
+            ),
+            textAlign: TextAlign.center,
+            softWrap: false,
+            maxLines: 5,
+            overflow: TextOverflow.ellipsis,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    "John Doe",
-                    style: GoogleFonts.rubik(
-                      fontSize: 28,
-                    ),
+                  const Icon(
+                    Icons.cake_outlined,
+                    size: 25,
                   ),
                   const SizedBox(
-                    width: 20,
+                    width: 5,
                   ),
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.cake_outlined,
-                            size: 25,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            "23.01.2000",
-                            style: GoogleFonts.rubik(
-                              fontSize: 18,
-                            ),
-                          )
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Icon(
-                            Icons.flag,
-                            size: 25,
-                          ),
-                          const SizedBox(
-                            width: 5,
-                          ),
-                          Text(
-                            "Afganistan",
-                            style: GoogleFonts.rubik(
-                              fontSize: 18,
-                            ),
-                          )
-                        ],
-                      ),
-                    ],
+                  Text(
+                    "23.01.2000",
+                    style: GoogleFonts.rubik(
+                      fontSize: 18,
+                    ),
                   )
                 ],
               ),
-            ),
-          ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.flag,
+                    size: 25,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Afganistan",
+                    style: GoogleFonts.rubik(
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
         ],
       ),
     );
--- lib/pages/profile_page/profile_page_widgets/statistics_section/statistics_section.dart
+++ lib/pages/profile_page/profile_page_widgets/statistics_section/statistics_section.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:mobile/pages/profile_page/profile_page_widgets/statistics_section/statistic_section_data.dart';
 
