1e04421173ff24343a2cbd750af4a2e5fa4631d2
Mariusz Wróbel <komodo454@wp.pl>
2023-08-05 17:41:14 +0200

    Add personal info section

--- lib/pages/profile_page/profile_page.dart
+++ lib/pages/profile_page/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/custom_widgets/custom_drawer/custom_drawer.dart';
 import 'package:mobile/custom_widgets/main_page_app_bar.dart';
+import 'package:mobile/pages/profile_page/profile_page_widgets/personal_info_section/personal_info_section.dart';
 import 'package:mobile/pages/profile_page/profile_page_widgets/profile_pictures_stack/profile_pictures_stack.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -12,16 +13,26 @@ class ProfilePage extends StatelessWidget {
       appBar: const MainPageAppBar(),
       drawer: CustomDrawer(context),
       body: Container(
+        constraints: const BoxConstraints(
+          minWidth: 0,
+          minHeight: 0,
+          maxWidth: double.infinity,
+          maxHeight: double.infinity,
+        ),
         padding: const EdgeInsets.all(30),
         child: const Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             ProfilePicturesStack(),
+            SizedBox(height: 70,),
+            PersonalInfoSection(),
           ],
         ),
       ),
     );
   }
 }
+
 
 
 
---
+++ lib/pages/profile_page/profile_page_widgets/personal_info_section/personal_info_section.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PersonalInfoSection extends StatelessWidget {
+  const PersonalInfoSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "John Doe",
+            style: GoogleFonts.rubik(
+              fontSize: 28,
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.cake_outlined,
+                    size: 25,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "23.01.2000",
+                    style: GoogleFonts.rubik(
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.flag,
+                    size: 25,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Afganistan",
+                    style: GoogleFonts.rubik(
+                      fontSize: 18,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
