f2eb833ed41e723a113bc4ea40a4b76e9c00200b
ArekStencel <hs.arek@wp.pl>
2023-09-29 22:50:46 +0200

    validations base

--- lib/custom_widgets/custom_form_filed/custom_form_field.dart
+++ lib/custom_widgets/custom_form_filed/custom_form_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile/custom_widgets/custom_form_filed/styled.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CustomFormField extends StatefulWidget {
   CustomFormField({
@@ -13,6 +14,8 @@ class CustomFormField extends StatefulWidget {
     this.isRequired = true,
     this.maxLength,
     this.initialValue,
+    this.validator,
+    this.onChanged,
   }) : super(key: key);
 
   final String? hintText;
@@ -23,6 +26,8 @@ class CustomFormField extends StatefulWidget {
   final bool isRequired;
   final int? maxLength;
   final String? initialValue;
+  final FormFieldValidator<String>? validator;
+  final ValueChanged<String>? onChanged;
 
   @override
   _CustomFormFieldState createState() => _CustomFormFieldState();
@@ -43,16 +48,37 @@ class _CustomFormFieldState extends State<CustomFormField> {
     });
   }
 
+  bool _isValidEmail(String email) {
+    final RegExp emailRegex = RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    return emailRegex.hasMatch(email);
+  }
+
+  bool _isStrongPassword(String password) {
+    // At least one upper case letter, one lower case letter, and either one number or one special character, and be at least 8 characters long
+    final RegExp passwordRegex = RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[\d@$!%*?&])[A-Za-z\d@$!%*?&]{8,}');
+    return passwordRegex.hasMatch(password);
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      validator: (value) {
-        if (widget.isRequired && (value == null || value.isEmpty)) {
-          return 'Please enter some value';
-        }
-        return null;
-      },
+      onChanged: widget.onChanged,
+      validator: widget.validator ?? (value) {
+          if (widget.isRequired && (value == null || value.isEmpty)) {
+            return 'Please enter some value';
+          }
+
+          if (widget.hintText == AppLocalizations.of(context).email && !_isValidEmail(value!)) {
+            return 'Enter a valid email';
+          }
+
+          if (widget.hintText == AppLocalizations.of(context).password && !_isStrongPassword(value!)) {
+            return 'Provide strong password';
+          }
+
+          return null;
+        },
       initialValue: widget.initialValue,
       style: GoogleFonts.rubik(fontSize: 14),
       cursorColor: Theme.of(context).colorScheme.secondary,
@@ -75,4 +101,5 @@ class _CustomFormFieldState extends State<CustomFormField> {
           : customTextFieldDecoration(context, widget.hintText, widget.inputIcon),
     );
   }
+
 }
