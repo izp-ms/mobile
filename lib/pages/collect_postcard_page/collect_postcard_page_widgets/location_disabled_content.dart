24adbbe4b619accc7f92701b692464bd6b4c9af7
Mariusz Wróbel <komodo454@wp.pl>
2023-10-01 15:26:24 +0200

    Fix style of location disabled page

--- lib/pages/collect_postcard_page/collect_postcard_page_widgets/location_disabled_content.dart
+++ lib/pages/collect_postcard_page/collect_postcard_page_widgets/location_disabled_content.dart
@@ -9,35 +9,93 @@ class LocationDisabledContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-            child: Text(
-              "Location is disabled",
-              style: GoogleFonts.rubik(
-                fontSize: 30,
-              ),
-            ),
+    return SingleChildScrollView(
+      child: Align(
+        alignment: Alignment.topCenter,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: 800,
           ),
-          const Icon(
-            Icons.location_off_outlined,
-            size: 200,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-            child: AutoSizeText(
-              "If you want to collect postcards you need to turn on your location to determine which postcards are near you.",
-              style: GoogleFonts.rubik(
-                fontSize: 22,
-              ),
-              textAlign: TextAlign.center,
-              maxLines: 5,
-            ),
-          ),
-        ],
+          child: isPhoneInLandscape(context)
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Flexible(
+                      flex: 2,
+                      child: Center(
+                        child: Icon(
+                          Icons.location_off_outlined,
+                          size: 200,
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: Text(
+                                "Location is disabled",
+                                style: GoogleFonts.rubik(
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                            AutoSizeText(
+                              "If you want to collect postcards you need to turn on your location to determine which postcards are near you.",
+                              style: GoogleFonts.rubik(
+                                fontSize: 22,
+                              ),
+                              textAlign: TextAlign.center,
+                              maxLines: 5,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+                      child: Text(
+                        "Location is disabled",
+                        style: GoogleFonts.rubik(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                    const Icon(
+                      Icons.location_off_outlined,
+                      size: 200,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                      child: AutoSizeText(
+                        "If you want to collect postcards you need to turn on your location to determine which postcards are near you.",
+                        style: GoogleFonts.rubik(
+                          fontSize: 22,
+                        ),
+                        textAlign: TextAlign.center,
+                        maxLines: 5,
+                      ),
+                    ),
+                  ],
+                ),
+        ),
       ),
     );
+  }
+
+  bool isPhoneInLandscape(BuildContext context) {
+    return MediaQuery.of(context).orientation == Orientation.landscape ||
+        MediaQuery.of(context).size.width > 600;
   }
 }
