dec60365c8063dbc91223260a1b4fa151d971aed
Mariusz Wróbel <komodo454@wp.pl>
2023-11-02 19:36:18 +0100

    Add action to theme switch

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/helpers/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  late bool _isDark;
+  bool get isDark => _isDark;
+
+  ThemeProvider() {
+    _isDark = false;
+    getPreferences();
+  }
+
+  set isDark(bool value) {
+    print("piesek");
+    _isDark = value;
+    AppSharedPreferences.saveThemePreference(value);
+    notifyListeners();
+  }
+
+  getPreferences() async {
+    _isDark = await AppSharedPreferences.getThemePreference();
+    notifyListeners();
+  }
+}
